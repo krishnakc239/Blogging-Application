4259be0887d89752f6c0e3475031ae867b185b55
krishnakc239 <krishnakc2339@gmail.com>
2019-07-24 10:26:07 -0500

    repositories added

---
+++ src/main/java/com/edu/mum/repository/PostRepository.java
@@ -0,0 +1,17 @@
+package com.edu.mum.repository;
+
+import com.edu.mum.domain.Post;
+import com.edu.mum.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByUserOrderByCreateDateDesc(User user, Pageable pageable);
+
+    Page<Post> findAllByOrderByCreateDateDesc(Pageable pageable);
+
+    Optional<Post> findById(Long id);
+}
---
+++ src/main/java/com/edu/mum/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.edu.mum.repository;
+
+import com.edu.mum.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String name, String pass);
+}
