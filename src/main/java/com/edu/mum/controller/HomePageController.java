011ef20e0ac3b8485059a1754165c54aec437465
krishnakc239 <krishnakc2339@gmail.com>
2019-07-24 10:24:20 -0500

    controllers added

---
+++ src/main/java/com/edu/mum/controller/HomePageController.java
@@ -0,0 +1,42 @@
+package com.edu.mum.controller;
+
+import com.edu.mum.domain.Post;
+import com.edu.mum.service.PostService;
+import com.edu.mum.util.Pager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomePageController {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping(value={"/"},
+            method= RequestMethod.GET)
+    public String homepage()
+    {
+        return "redirect:/index";
+    }
+
+
+    @GetMapping("/index")
+    public String home(@RequestParam(defaultValue = "0") int page,
+                       Model model) {
+
+        Page<Post> posts = postService.findAllOrderedByDatePageable(page);
+        Pager pager = new Pager(posts);
+
+        model.addAttribute("pager", pager);
+
+        return "views/home/index";
+    }
+
+    }
+
---
+++ src/main/java/com/edu/mum/controller/LoginController.java
@@ -0,0 +1,70 @@
+package com.edu.mum.controller;
+
+import com.edu.mum.domain.User;
+import com.edu.mum.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value={"/login"}, method = RequestMethod.GET)
+    public String login(){
+        return "/views/home/login";
+    }
+
+
+    @RequestMapping(value="/registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("/views/home/registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findByEmail(user.getEmail()).get();
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("/views/home/registration");
+        } else {
+            userService.save(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("/views/home/registration");
+
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/admin/index", method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByEmail(auth.getName()).get();
+        modelAndView.addObject("userName", "Welcome " + user.getFirstName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("/views/home/home");
+        return modelAndView;
+    }
+
+
+}
