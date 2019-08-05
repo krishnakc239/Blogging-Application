16bfee2a04eaea61c680cb7a5844bdbe4bbbdada
krishnakc239 <krishnakc2339@gmail.com>
2019-08-05 00:53:53 -0500

    lombok @Data used in Post model

--- src/main/java/com/edu/mum/domain/Post.java
+++ src/main/java/com/edu/mum/domain/Post.java
@@ -1,5 +1,6 @@
 package com.edu.mum.domain;
 
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -10,7 +11,7 @@ import javax.validation.constraints.NotNull;
 import java.sql.Blob;
 import java.util.Collection;
 import java.util.Date;
-
+@Data
 @Entity
 @Table(name = "posts")
 public class Post {
@@ -27,7 +28,7 @@ public class Post {
 
     @Lob
     @Column(name = "body", columnDefinition = "TEXT")
-    @NotNull(message = "*Please privde the content")
+    @NotNull(message = "*Please provide the content")
     private String body;
     private double avgRating;
     private int ratedCount;
@@ -51,54 +52,6 @@ public class Post {
 
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date date) {
-        this.createDate = date;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Collection<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(Collection<Comment> comments) {
-        this.comments = comments;
-    }
-
     public void updateRatedCount() {
         this.ratedCount++;
     }
@@ -114,27 +67,4 @@ public class Post {
         System.out.println("final user ratin = "+ getAvgRating());
     }
 
-    public int getRatedCount() {
-        return ratedCount;
-    }
-
-    public double getAvgRating() {
-        return avgRating;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
 }
