6c2b0cbba0f54fc36be843fdddf25f96d730d4d8
krishnakc239 <krishnakc2339@gmail.com>
2019-08-25 22:54:11 -0500

    comment count logic added.

--- src/main/java/com/edu/mum/service/CommentService.java
+++ src/main/java/com/edu/mum/service/CommentService.java
@@ -2,11 +2,13 @@ package com.edu.mum.service;
 
 import com.edu.mum.domain.Comment;
 import com.edu.mum.domain.Post;
+import com.edu.mum.domain.User;
 
 import java.util.List;
 
 public interface CommentService {
 
     Comment save(Comment comment);
-   List<Comment> findFirst5ByPost(Post post);
+    List<Comment> findFirst5ByPost(Post post);
+    int getCommentCountForUser(User user);
 }
